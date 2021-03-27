71e956cece7d22eb40837ce72d0ba7e36d27bc1c
Paweł Płonka <pjplonka@gmail.com>
2021-03-27 13:38:53 +0100

    Add two new base inputs: Text input and Select.

---
+++ src/common/components/form/TextInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-form-group :label="label">
+    <b-form-input
+        @input="$emit('input', $event)"
+        :value="value"
+    ></b-form-input>
+    <span v-if="errors" class="errors">{{ errors[0] }}</span>
+    <b-form-text v-if="hint">{{ hint }}</b-form-text>
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  name: "TextInput",
+  props: {
+    value: {
+      type: String,
+      default: null
+    },
+    errors: {
+      type: Array,
+      default: () => []
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    hint: {
+      type: String,
+      default: ''
+    },
+  },
+}
+</script>
+<style scoped>
+.errors {
+  font-size: 80%;
+  color: red;
+}
+</style>
