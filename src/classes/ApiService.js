8dc7e69f10004ff3941f11f6fba85949f745718b
Paweł Płonka <pjplonka@gmail.com>
2021-03-30 14:20:51 +0200

    Use auth token in ApiService for auth header.

--- src/classes/ApiService.js
+++ src/classes/ApiService.js
@@ -2,15 +2,23 @@ import axios from 'axios'
 
 export default class ApiService {
     static get(uri) {
-        return axios.get(process.env.VUE_APP_API_URL + uri)
+        return axios.get(process.env.VUE_APP_API_URL + uri, this.headers())
     }
     static post(uri, data) {
-        return axios.post(process.env.VUE_APP_API_URL + uri, data)
+        return axios.post(process.env.VUE_APP_API_URL + uri, data, this.headers())
     }
     static put(uri, data) {
-        return axios.put(process.env.VUE_APP_API_URL + uri, data)
+        return axios.put(process.env.VUE_APP_API_URL + uri, data, this.headers())
     }
     static delete(uri) {
-        return axios.delete(process.env.VUE_APP_API_URL + uri)
+        return axios.delete(process.env.VUE_APP_API_URL + uri, this.headers())
+    }
+
+    static headers() {
+        return {
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem('auth-token') || ''
+            }
+        }
     }
 }
